0b8c8d69aeb3579bcee22067d363498175c83cd8
Kevin Chen & ian bevel <student@epicodus.com>
2015-06-02 16:24:29 -0700

    Add forms for movie and ticket

--- js/scripts.js
+++ js/scripts.js
@@ -25,8 +25,68 @@ Ticket.prototype.price = function() {
   return total;
 }
 
-function Movie(movieTitle, release, showTime) {
+function Movie(movieTitle, release) {
   this.movieTitle = movieTitle;
   this.release = release;
-  this.showTime = showTime;
+  this.showTimes = [];
 }
+
+function resetFields() {
+  $("input#release-type").val("")
+  $("input#new-movie-title").val("")
+  $("input#new-show-time").val("")
+
+}
+
+$(document).ready(function() {
+  var allMovies = [];
+  $("#add-showtime").click(function(event) {
+    event.preventDefault();
+    $(".new-show-time-container").last().clone().appendTo("#new-showtimes");
+    $(".new-address").last().find("input").each(function(){
+      $(this).val("");
+    });
+  });
+
+  $("form#add-movie").submit(function(event) {
+    event.preventDefault();
+
+    var movieTitle = $("input#new-movie-title").val();
+    var releaseType = $("select#release-type").val();
+    var myMovie = new Movie(movieTitle, releaseType);
+
+    $(".new-show-time").each(function() {
+      myMovie.showTimes.push($(this).val());
+    });
+
+    $("ul.movies").append("<li class='movie-item'>" + myMovie.movieTitle + "</li>");
+
+    $(".movie-item").last().click(function() {
+      $(".movie-info").text(myMovie.movieTitle);
+      myMovie.showTimes.forEach(function(showTime) {
+        $("ul.movie-info-times").append("<li>" + showTime.toString() + "</li>")
+      });
+      var index = allMovies.indexOf(myMovie)
+      $("form#buy-ticket").data("id", index);
+      $("form#buy-ticket").data("release", myMovie.release);
+
+      myMovie.showTimes.forEach(function(showTime) {
+        $("select#ticket-times").append("<option value='" + showTime.toString() + "'>" + showTime.toString() + "</option>");
+      });
+    });
+
+    allMovies.push(myMovie);
+    resetFields();
+  });
+
+  $("form#buy-ticket").submit(function(event) {
+    event.preventDefault();
+    var release = $("form#buy-ticket").data("release");
+    var showTime = $("select#ticket-times").val();
+    var age = $("select#age").val();
+
+    var myTicket = new Ticket(release, showTime, age);
+    $("p#price").text(myTicket.price().toString());
+  })
+
+});
